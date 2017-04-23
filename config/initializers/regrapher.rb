25ad48aa610c88d7617a29471bdc0da9297ddbcd
Ramzi Ben Salah <ramzi.salah@gmail.com>
2017-04-23 22:39:39 +0200

    send full resolution process_action metric values

--- config/initializers/regrapher.rb
+++ config/initializers/regrapher.rb
@@ -27,9 +27,11 @@ Warden::Manager.before_logout { Regrapher.client.increment('auth.sign_out') }
 #
 ActiveSupport::Notifications.subscribe 'process_action.action_controller' do |name, started, finished, unique_id, data|
   next unless data[:status] || data[:exception]
+
+  Regrapher.client.gauge("#{name}.view_runtime", data[:view_runtime].to_f)
+  Regrapher.client.gauge("#{name}.db_runtime", data[:db_runtime].to_f)
+  Regrapher.client.gauge("#{name}.runtime", (finished - started) * 1000.0)
+
   status = data[:status] || Rack::Utils.status_code(ActionDispatch::ExceptionWrapper.rescue_responses[data[:exception][0]])
-  Regrapher.client.gauge("#{name}.view_runtime", data[:view_runtime].round(2)) if data[:view_runtime]
-  Regrapher.client.gauge("#{name}.db_runtime", data[:db_runtime].round(2)) if data[:db_runtime]
-  Regrapher.client.gauge("#{name}.runtime", ((finished - started) * 1000).round(2))
   Regrapher.client.increment("#{name}.status_#{status}")
 end
